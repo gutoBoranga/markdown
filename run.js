fa75078a6aaacea6974304c5696c2c255e168701
Augusto Boranga <guto.boranga@gmail.com>
2018-02-27 04:09:05 +0000

    Update run.js

--- run.js
+++ run.js
@@ -1,13 +1,33 @@
 var markdownpdf = require("markdown-pdf")
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  var port = app.get('port').toString();
+  port = port.split('').join(' ');
+});
 
 var options = {
     highlightCssPath: 'style.css',
     paperOrientation: 'landscape',
     paperBorder: '0px',
 }
-
-console.log("Converting ...");
+ 
+app.get('/', function (req, res) {
+    console.log("Converting ...");
 
 markdownpdf(options).from("input.md").to("output.pdf", function () {
   console.log("Done")
-})
+    var data =fs.readFileSync('./output.pdf');
+res.contentType("application/pdf");
+res.send(data)
+});
+    
+});
+
+app.get('/test', function(req, res) {
+    res.send('i am fine');
+});
+ 
